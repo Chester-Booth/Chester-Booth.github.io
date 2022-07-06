dc734b4caf812e874e7b3f1713e5429ea45d7db6
TheCraftBlox <105993308+TheCraftBlox@users.noreply.github.com>
2022-07-06 20:25:17 +0100

    Add Mount Mount

---
+++ Worlds/Isometric/MountMount/overviewerConfig.js
@@ -0,0 +1,233 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Mount. Mount - overworld"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1657132786",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Day Upper Right",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "Mount. Mount - day Upper-Right",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Mount. Mount - overworld",
+            "last_rendertime": 1657122265,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                1033,
+                0,
+                1153
+            ],
+            "minZoom": 0,
+            "spawn": [
+                1033,
+                0,
+                1153
+            ],
+            "north_direction": 1
+        },
+        {
+            "name": "Day Upper Left",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "Mount. Mount - day Upper-Left",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Mount. Mount - overworld",
+            "last_rendertime": 1657122265,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                1033,
+                0,
+                1153
+            ],
+            "minZoom": 0,
+            "spawn": [
+                1033,
+                0,
+                1153
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Day Lower Right",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "Mount. Mount - day Lower-Right",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Mount. Mount - overworld",
+            "last_rendertime": 1657122265,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                1033,
+                0,
+                1153
+            ],
+            "minZoom": 0,
+            "spawn": [
+                1033,
+                0,
+                1153
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "Day Lower Left",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "Mount. Mount - day Lower-Left",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Mount. Mount - overworld",
+            "last_rendertime": 1657122265,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                1033,
+                0,
+                1153
+            ],
+            "minZoom": 0,
+            "spawn": [
+                1033,
+                0,
+                1153
+            ],
+            "north_direction": 3
+        },
+        {
+            "name": "Cave Lower Right",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "Mount. Mount - cave Lower-Right",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Mount. Mount - overworld",
+            "last_rendertime": 1657122265,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                1033,
+                0,
+                1153
+            ],
+            "minZoom": 0,
+            "spawn": [
+                1033,
+                0,
+                1153
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "Night Upper Left",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "Mount. Mount - Night Upper-Left",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Mount. Mount - overworld",
+            "last_rendertime": 1657122265,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                1033,
+                0,
+                1153
+            ],
+            "minZoom": 0,
+            "spawn": [
+                1033,
+                0,
+                1153
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Night Lower Right",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "Mount. Mount - Night Lower-Right",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Mount. Mount - overworld",
+            "last_rendertime": 1657122265,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                1033,
+                0,
+                1153
+            ],
+            "minZoom": 0,
+            "spawn": [
+                1033,
+                0,
+                1153
+            ],
+            "north_direction": 2
+        }
+    ]
+};
